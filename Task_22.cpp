92cee583f0d74c8e9a0d492d3d411306a68ebe76
moldova111 <99029283+moldova111@users.noreply.github.com>
2026-03-17 13:17:17 +0800

    Task_22

---
+++ Task_22.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    setlocale(0, "Russian");
+    ifstream f("output.txt");
+    int x;
+    int i = 1;
+    while (f >> x) {
+        cout << i << ". " << x << endl;
+        i++;
+    }
+    f.close();
+    return 0;
+}
