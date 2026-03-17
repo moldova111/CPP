cf72ef6bf5b5f48cc0e34f39e0c01e370e827555
moldova111 <99029283+moldova111@users.noreply.github.com>
2026-03-17 11:29:29 +0800

    Task_02

---
+++ Task_02.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    setlocale(0, "Russian");
+    int x, y;
+    cin >> x;
+    cin >> y;
+    int z = x / y;
+    int v = x % y;
+    cout << z << " Результат деления нацело" << endl;
+    cout << v << " Остаток";
+    return 0;
+}
+
