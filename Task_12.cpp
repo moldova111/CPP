6e1bc686fb5284168098b8d400939b3891997faf
moldova111 <99029283+moldova111@users.noreply.github.com>
2026-03-17 11:45:53 +0800

    Task_12

---
+++ Task_12.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "Russian");
+    int a = 0;
+    int i = 1;
+    while (i <= 1000) {
+        if (i % 7 == 0) {
+            a = a + i;
+            cout << i << endl;
+
+        }
+        i = i + 4;
+    }
+    cout << a << " Сумма всех найденных чисел";
+    return 0;
+}
+
