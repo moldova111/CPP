cfb8801c38528b2e71fb99347398c5c1f3b56ff4
moldova111 <99029283+moldova111@users.noreply.github.com>
2026-03-17 11:18:47 +0800

    Task_01

---
+++ Task_01.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int a = 2;
+    float b = 5.1;
+    double c = 4.32;
+    long d = 321345544;
+    cout << a << '\n';
+    cout << b << '\n';
+    cout << c << '\n';
+    cout << d << '\n';
+    int x, y;
+    cin >> x;
+    cin >> y; 
+    cout << x + y << '\n';
+    cout << x * y << '\n';
+    cout << x - y << '\n';
+    cout << x / y << '\n';
+    return 0;
+}
+
