86f6e0b6b10e9a837fefacf55bda5657579fcc8b
moldova111 <99029283+moldova111@users.noreply.github.com>
2026-03-17 13:09:45 +0800

    Task_21

---
+++ Task_21.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+	setlocale(0, "Russian");
+	int A, B;
+	cin >> A;
+	cin >> B;
+	while (A > B) {
+		cout << "Ошибка, B должно быть больше A, Введите B заново";
+		cin >> B;
+	}
+	ofstream f("output.txt");
+	
+	for (int i = A; i <= B; i++) {
+		f << i * 3 << endl;
+	}
+	f.close();
+	return 0;
+}
+
